8eafb6681c21778837a66c1391129b906287a089
“mbt1” <mbt1@users.noreply.github.com>
2023-09-30 02:35:57 +0000

    started nodejs configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 bundle lock --add-platform ruby
 bundle lock --add-platform x86_64-linux
+export PATH=$PATH:$(dirname $(which npm))
+echo 'export PATH=$PATH:$(dirname $(which npm))' >> ~/.bashrc
