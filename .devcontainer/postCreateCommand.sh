0e912c62be34b998d48718b1312f33e99f545ce4
“mbt1” <mbt1@users.noreply.github.com>
2023-10-01 13:24:51 -0400

    start over

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bundle lock --add-platform ruby
-bundle lock --add-platform x86_64-linux
-export PATH=$PATH:$(dirname $(which npm))
-echo 'export PATH=$PATH:$(dirname $(which npm))' >> ~/.bashrc
--- README.md
+++
@@ -1,2 +0,0 @@
-# password
-A simple Password Generator
