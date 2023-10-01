12748da8b83ba50974d2ca3711cd88a7f330c637
“mbt1” <mbt1@users.noreply.github.com>
2023-10-01 18:10:15 +0000

    added react, Gatsby, TypeScript, and SASS to devcontainer

--- .devcontainer/gatsby/install.sh
+++ .devcontainer/gatsby/install.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:$(dirname $(which npm))
 npm install
-npm install gatsby
-npm install gatsby-cli
-npm install gh-pages --save-dev
+npm install -g gatsby
+npm install -g gatsby-cli
+npm install -g gh-pages
